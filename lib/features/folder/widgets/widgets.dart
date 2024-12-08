15ce3544fc67df9491cb04e097002e70c5bd35c8
vosandr <61714173+vosandr@users.noreply.github.com>
2024-12-08 14:06:06 +0300

    Delete lib directory

--- lib/features/folder/widgets/widgets.dart
+++
@@ -1,3 +0,0 @@
-export 'app_bar_folder.dart';
-export 'context_menu_folder.dart';
-export 'folder_page.dart';
--- lib/features/pages.dart
+++
@@ -1,2 +0,0 @@
-export 'file/widgets/file.dart';
-export 'folder/widgets/folder_page.dart';
